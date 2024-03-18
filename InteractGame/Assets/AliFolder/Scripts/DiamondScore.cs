7c93acf2702ee4132c93dc3f0d24fb05cc1fa9fe
Aly Sharaf <ali.charaf59@gmail.com>
2024-03-18 07:02:13 +0400

    score also increases by hitting bumpers, wall bumpers, and bouncers. Added more instructions in the pause menu

--- InteractGame/Assets/AliFolder/Scripts/DiamondScore.cs
+++ InteractGame/Assets/AliFolder/Scripts/DiamondScore.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class DiamondScore : MonoBehaviour
 {
-      public int scoreValue = 100; // Score value to increase when collected
+      public int bouncerScore = 1; // Score value when the ball hits a bouncer
+    public int bumperScore = 2; // Score value when the ball hits a bumper or wall bumper
+    public int diamondScore = 5; // Score value when the ball collects a diamond
 
     ScoreManager scoreManager; // Reference to the ScoreManager script
 
@@ -17,16 +19,33 @@ public class DiamondScore : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the collider that triggered the diamond is the ball
-        if (other.CompareTag("Ball"))
+        if (other.CompareTag("Ball") && gameObject.CompareTag("Diamond"))
         {
             // Increase the score using the ScoreManager
-            scoreManager.IncrementScore(scoreValue);
+            scoreManager.IncrementScore(diamondScore);
 
             // Deactivate the diamond GameObject
             gameObject.SetActive(false);
 
             // Start a coroutine to respawn the diamond after a delay
             DiamondRespawnManager.Instance.StartRespawnCoroutine(gameObject, 5f); // Adjust delay as needed
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            if (gameObject.CompareTag("Bouncer"))
+            {
+                // Increase the score using the ScoreManager
+                scoreManager.IncrementScore(bouncerScore);
+            }
+            else if (gameObject.CompareTag("Bumper") || gameObject.CompareTag("WallBumper"))
+            {
+                // Increase the score using the ScoreManager
+                scoreManager.IncrementScore(bumperScore);
+            }
         }
     }
 }
